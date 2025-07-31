bee028e89321b4873ac58d92407092d38fc91cae
vamshi <vamshi2670@gmail.com>
2025-07-31 12:08:37 +0530

    first changes

---
+++ src/portfolio/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3 className="footer-name">Vamshi</h3>
+      <p>© 2025 Vamshi. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
